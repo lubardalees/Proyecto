db9832fda8aa6b1044c4832ef151a50e6296abf4
Lwanna <lcorrea@estudiante.crackthecode.la>
2021-12-26 00:42:18 -0500

    proyecto actualizado 26/12

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,20 @@ if(window.location.href.includes("nombre.html")){
 
     document.getElementById("enviar").addEventListener("click", function(){enviar()});
 }
+
+
+
+const aplicacion = document.querySelector('.container')
+
+const url = 'https://ghibliapi.herokuapp.com/films'
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    data.forEach(usuario => {
+        const p = document.createElement('p') 
+        p.innerHTML = usuario.title
+        aplicacion.appendChild(p)
+    });
+})
+.catch(err => console.log(err))

3b6b0d5e71a5b5a25ab52a3c018ec1a91b7b5c3e
Lwanna <lcorrea@estudiante.crackthecode.la>
2021-12-27 13:08:13 -0500

    proyecto actualizado 27/12

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,4 +1,4 @@
-const aplication = document.querySelector('container')
+const aplication = document.querySelector('.container')
 const getUrl = new URLSearchParams(window.location.search)
 id = getUrl.get('id')
 const url = 'https://ghibliapi.herokuapp.com/films'
@@ -7,9 +7,20 @@ fetch(`${url}/${id}`)
 .then(res => res.json())
 .then(data => {
 
- const title = document.createElement('p')
+ const title = document.createElement('h4')
  title.innerHTML = data.title
+ const original_title = document.createElement('h5')
+ original_title.innerHTML = data.original_title
+ const description = document.createElement('h6')
+ description.innerHTML = data.description
+ const image = document.createElement('img')
+ image.src = data.image
  aplication.appendChild(title)
+ aplication.appendChild(original_title)
+ aplication.appendChild(description)
+ aplication.appendChild(image)
+
+
 
 
 })
